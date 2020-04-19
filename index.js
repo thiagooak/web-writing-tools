0a982d3fd1bfddbb9d7ca9ed4791e555fad3c96f
Thiago Carvalho <thiago.oak@gmail.com>
2020-04-19 17:00:17 +0200

    Semi-working version

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 import * as wasm from "wasm-writing-tools";
 
-wasm.greet();
+let output = document.getElementById("output");
+let submit = document.getElementById("submit");
+
+output.focus();
+submit.addEventListener("click", runChecks);
+output.addEventListener("blur", runChecks);
+// output.addEventListener("keyup", handleKeyUp);
+
+function handleKeyUp() {
+  // @TODO caret keeps moving to 0. set range and selection don't work as I expected
+  runChecks();
+}
+
+function runChecks() {
+  var re = new RegExp("(<[^>]*>)", "g");
+  let text = output.innerHTML.replace(re, "");
+
+  const result = wasm.check(text);
+
+  output.innerHTML = result;
+}
+
+runChecks();
