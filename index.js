c460412bec2d3cd722083c4499990f92cc98b85d
Thiago Carvalho <thiago.oak@gmail.com>
2020-04-22 08:50:03 +0200

    Moving back to two divs

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 import * as wasm from "wasm-writing-tools";
 
+let input = document.getElementById("input");
 let output = document.getElementById("output");
-let submit = document.getElementById("submit");
 
-output.focus();
-submit.addEventListener("click", runChecks);
-output.addEventListener("blur", runChecks);
-// output.addEventListener("keyup", handleKeyUp);
+input.focus();
+input.addEventListener("keyup", handleKeyUp);
 
 function handleKeyUp() {
-  // @TODO caret keeps moving to 0. set range and selection don't work as I expected
   runChecks();
 }
 
 function runChecks() {
   var re = new RegExp("(<[^>]*>)", "g");
-  let text = output.innerHTML.replace(re, "");
+  let text = input.innerHTML.replace(re, "");
 
   const result = wasm.check(text);
 
